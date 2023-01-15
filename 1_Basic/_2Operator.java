f0d682baf2d2aa14ddf2b2cad6aa3acd63efe98b
AshrafRahul <m.ashraful.islam007@gmail.com>
2023-01-15 23:58:17 +0600

    Java || basic

---
+++ 1_Basic/_2Operator.java
@@ -0,0 +1,14 @@
+public class _2Operator {
+    public static void main(String[] args){
+        int a=10,b=2;
+        int sum = a+b;
+        int sub = a-b;
+        int mul = a*b;
+        int div = a/b;
+
+        System.out.println(sum);
+        System.out.println(sub);
+        System.out.println(mul);
+        System.out.println(div);
+    }
+}
