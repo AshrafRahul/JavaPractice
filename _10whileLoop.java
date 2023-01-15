70946ad281d8c6b4e63349023ed10fe37f4fcc40
AshrafRahul <m.ashraful.islam007@gmail.com>
2023-01-15 12:30:09 +0600

    Java || loop

---
+++ _10whileLoop.java
@@ -0,0 +1,9 @@
+public class _10whileLoop {
+    public static void main(String[] args){
+        int i=1;
+        while(i<11){
+            System.out.print(i+" ");
+            i++;
+        }
+    }
+}
