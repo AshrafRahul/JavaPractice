2e2e09a099b00ccb6629e4ec22a3fc7f792f9b60
AshrafRahul <m.ashraful.islam007@gmail.com>
2023-02-09 12:41:00 +0600

    1

--- OOP/method.java
+++ OOP/method.java
@@ -1,21 +1,27 @@
 package JavaPractice.OOP;
 
-public class class_object {
+public class method {
     public static void main(String[] args) {
         Person p1 = new Person();
         p1.name = "Ashraf";
         p1.age = 22;
+        p1.eat();
 
         Person p2 = new Person();
         p2.name = "Rahul";
         p2.age = 23;
-
-        System.out.println(p1.name+" "+p1.age);
-        System.out.println(p2.name+" "+p2.age);
+        p2.walk();
     }
 }
 
 class Person{
     String name;
     int age;
+
+    void walk(){
+        System.out.println(name+" is walking");
+    }
+    void eat(){
+        System.out.println(name+" is eating");
+    }
 }
